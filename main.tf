e8962acf9fcc21ea3207197f6f34eeecdee1bdfe
rganesh29 <151710631+rganesh29@users.noreply.github.com>
2024-07-15 12:53:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,18 @@ provider "aws" {
   region = "us-east-1"  # Specify your AWS region
 }
 
-# Create a security group allowing all TCP traffic
-resource "aws_security_group" "allow_all_tcp" {
-  name        = "allow_all_tcp"
-  description = "Allow all TCP traffic"
-  vpc_id      = data.aws_vpc.default.id
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_security_group" "instance" {
+  vpc_id = aws_vpc.main.id
 
   ingress {
     from_port   = 0
@@ -21,22 +28,26 @@ resource "aws_security_group" "allow_all_tcp" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "allow-all-tcp"
+  }
 }
 
-# Launch an EC2 instance with the specified security group
 resource "aws_instance" "web" {
   ami                    = "ami-04a81a99f5ec58529"  # Replace with your desired AMI ID
   instance_type          = "t2.medium"
-  security_groups        = [aws_security_group.allow_all_tcp.name]
+  subnet_id              = aws_subnet.main.id
+  security_groups        = [aws_security_group.instance.name]
   associate_public_ip_address = true
 
   user_data = <<-EOF
-                #!/bin/bash
-                apt-get update
-                apt-get install -y apache2 unzip
-                systemctl start apache2
-                systemctl enable apache2
-                EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2 unzip
+              systemctl start apache2
+              systemctl enable apache2
+              EOF
 
   tags = {
     Name = "web-server"
@@ -46,3 +57,4 @@ resource "aws_instance" "web" {
 output "instance_public_ip" {
   value = aws_instance.web.public_ip
 }
+
