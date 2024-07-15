54b0b8f1f0faaec4d3d2d664655f5b481ac788f1
rganesh29 <151710631+rganesh29@users.noreply.github.com>
2024-07-15 15:38:47 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "bootcamp-tf-statefiles"
+    bucket         = "bootcamp-tf-statefiles-1"
     key            = "terraform.tfstate"
     region         = "us-east-1"  # Replace with your bucket's region
   }
